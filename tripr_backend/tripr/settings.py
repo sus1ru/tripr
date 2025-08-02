919c4b39b1ebcfa6d5c88eac6955ab5d92ff1b10
sus1ru <sus1ru.s4n@gmail.com>
2025-08-02 17:22:28 +0545

    feat: added location in installed apps

--- tripr_backend/tripr/settings.py
+++ tripr_backend/tripr/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user",
+    "location",
     "django_extensions",
     "rest_framework",
     "rest_framework.authtoken",
